ca5fc82f409dc22b05d640fdb4d8a67fd1c437ca
안지호 <dkswlgh99@naver.com>
2022-01-02 23:36:15 +0900

    1059

--- baekjoon/1059.py
+++ baekjoon/1059.py
@@ -3,12 +3,25 @@ S = []
 S = input().split()
 n = int(input())
 sn = 0
-ln = 0
+ln = 1001
 for i in S :
     i = int(i)
     if i < n :
-        if sn > i : sn = i
+        if sn < i : sn = i
     elif i > n :
-        if ln < i : ln = i
+        if ln > i : ln = i
     elif i == n :
+        ln = -1
         break
+
+count = 0
+if ln == -1 : print(count)
+else :
+    while True :
+        sn += 1
+        if sn != n :
+            count += ln - n
+        elif sn == n :
+            count += ln - n - 1
+            break
+    print(count)
