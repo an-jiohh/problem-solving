78a81fd7215baccbb37d47bf7db6632fefbb96fb
안지호 <dkswlgh99@naver.com>
2022-01-04 01:01:31 +0900

    1159

--- baekjoon/1159.py
+++ baekjoon/1159.py
@@ -3,7 +3,21 @@ S = {}
 
 for i in range(n) :
     m = input()
-    if m[0] in S :  S[m[0]] = 1
-    else : S[m[0]] = S[m[0]] + 1
+    if m[0] in S :  
+        S[m[0]] = S[m[0]] + 1
+    else : 
+        S[m[0]] = 1
 
-print(S)
+count = 0
+name = []
+for i in S:
+    if S[i] >= 5 :
+        name.append(i)
+        count += 1
+
+if count == 0 :
+    print("PREDAJA")
+else :
+    name.sort()
+    for i in name :
+        print(i,end="")
