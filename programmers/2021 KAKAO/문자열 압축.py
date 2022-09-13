95b8799f0989a257d39824db982ada75fa2a05e4
an-jiohh <dkswlgh99@naver.com>
2022-09-13 20:14:54 +0900

    문자열 압축

--- programmers/2021 KAKAO/문자열 압축.py
+++ programmers/2021 KAKAO/문자열 압축.py
@@ -5,15 +5,16 @@ def solution(s):
         check = 0
         ch = s[0:0+i] #슬라이딩 윈도우
         sum = 0
-        for j in range(i, len(s) - i + 1, i) :
+        for j in range(i, len(s), i) :
             if ch == s[j : j+i] :
                 if check == 0 : sum += 1
                 temp = temp.replace(ch, "", 1)
-                check = 1
+                check += 1
+                if check == 9 : sum += 1
             else : check = 0
             ch = s[j : j+i] ## 다음슬라이드로 변경
         if min >= len(temp) + sum : min = len(temp) + sum
     return min
 
-t = "aaaaaaaaaaaabcd"
+t = "xxxxxxxxxxyyy"
 print(solution(t))
