05f00bdb93130920f5e6858a439943f8d5039282
an-jiohh <dkswlgh99@naver.com>
2022-09-13 16:43:38 +0900

    문자열 압축(실패)

--- baekjoon/1152.py
+++ baekjoon/1152.py
@@ -1,3 +1,1 @@
 print(len(input().split()))
-
-print(len(a))
---
+++ programmers/2021 KAKAO/문자열 압축.py
@@ -0,0 +1,19 @@
+def solution(s):
+    min = len(s)
+    for i in range(1,int(len(s)/2) + 1) : 
+        temp = s
+        check = 0
+        ch = s[0:0+i] #슬라이딩 윈도우
+        sum = 0
+        for j in range(i, len(s) - i + 1, i) :
+            if ch == s[j : j+i] :
+                if check == 0 : sum += 1
+                temp = temp.replace(ch, "", 1)
+                check = 1
+            else : check = 0
+            ch = s[j : j+i] ## 다음슬라이드로 변경
+        if min >= len(temp) + sum : min = len(temp) + sum
+    return min
+
+t = "aaaaaaaaaaaabcd"
+print(solution(t))
